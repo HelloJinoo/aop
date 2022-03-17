e70bd48368312ac4cb48a50a44add14578bede74
jinwoo <wkdwlsdn9637@naver.com>
2022-03-17 21:53:16 +0900

    스프링 aop 개념 및 구현

---
+++ src/main/java/hello/aop/AopApplication.java
@@ -0,0 +1,20 @@
+package hello.aop;
+
+import hello.aop.order.aop.AspectV1;
+import hello.aop.order.aop.AspectV3;
+import hello.aop.order.aop.AspectV5Order;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+@SpringBootApplication
+//@Import(AspectV1.class)
+//@Import(AspectV3.class)
+@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxApsect.class})
+public class AopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopApplication.class, args);
+	}
+
+}
